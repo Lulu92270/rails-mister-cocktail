27e2c4358169c31772fb55b5a5449f563453e218
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-05 15:46:51 +0000

    seed issue

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,9 @@
 class CocktailsController < ApplicationController
+   def index
+     @cocktails = Cocktail.all.order(name: :asc)
+   end
+
+   def show
+    @cocktail = Cocktail.find(params[:id])
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying everything! 😈"
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
+puts "creating ingredients..."
+
+Cocktail.create(name: "1")
+Cocktail.create(name: "2")
+Cocktail.create(name: "3")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts 'Creating cocktails...'
+
+10.times do
+
+  # cock = Cocktail.create(
+    
+  #   name: Faker::Beer.unique.name
+  # )
+
+  # 2.times do
+    # my_ingredient = Ingredient.new(name: ['lemon', 'ice', 'mint leaves'].sample)
+    my_dose = Dose.create!(description: Faker::Cannabis.health_benefit, cocktail: Cocktail.all[rand(0..2)], ingredient: Ingredient.all[rand(0..2)])
+
+    # my_dose.ingredient = my_ingredient
+    # my_dose.cocktail = cock
+
+  # end
+end
+
+puts 'Done!'
