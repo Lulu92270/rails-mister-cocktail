ed4f5d8dd0f0c7fbe3f32babfa3659d467337eae
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-05 12:55:11 +0000

    rake successfull

--- db/migrate/20201105112548_create_doses.rb
+++ db/migrate/20201105112548_create_doses.rb
@@ -1,8 +1,10 @@
 class CreateDoses < ActiveRecord::Migration[6.0]
   def change
     create_table :doses do |t|
-      t.string :description
+      t.string :description, null: false
 
+      t.references :ingredient, null: false, foreign_key: true
+      t.references :cocktail, null: false, foreign_key: true
       t.timestamps
     end
   end
